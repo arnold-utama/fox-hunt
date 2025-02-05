50cf275c804edb1789d2a883e7a08d5b46c3e6a2
arnold-utama <arnoldutama3@gmail.com>
2025-02-05 23:01:51 +0700

    revisi game

--- javascript/game.js
+++ javascript/game.js
@@ -1,56 +1,34 @@
 let totalTime = 20; // set waktu per round di sini
 let totalSwiper = Math.round(totalTime * 1.5); // jumlah Swiper yang muncul
 let totalBoots = Math.round(totalSwiper / 2); // jumlah Boots yang muncul
-let totalGoldSwiper = Math.round(totalSwiper / 5); // jumlah Boots yang muncul
+let totalZalvin = Math.round(totalSwiper / 5); // jumlah Zalvin yang muncul
 let swiperPoints = 100; // poin yang didapat ketika menembak Swiper
-let goldSwiperPoints = 500; // poin yang didapat ketika menembak Gold Swiper
+let zalvinPoints = 500; // poin yang didapat ketika menembak Zalvin
 let bootsPoints = -300; // poin yang didapat ketika menembak Boots
 let score = document.getElementById("score");
 let timer = document.getElementById("timer");
 timer.textContent = totalTime;
+let gameArea = document.getElementById("game-area");
+let gameAreaHeight = gameArea.offsetHeight - 150;
 let currentScore = 0;
 
-function createTarget(targetType) {
+function createSwiper() {
   let target = document.createElement("img");
-  let gameArea = document.getElementById("game-area");
-  let gameAreaHeight = gameArea.offsetHeight - 150;
-
   target.style.position = "absolute";
   target.style.top = Math.random() * gameAreaHeight + "px";
   target.style.height = "150px";
+  target.src = "./assets/swiper1.png";
+  target.alt = "Swiper";
+  let transition = "transform 5s ease-in-out";
 
-  let transition = "";
-  if (targetType === "swiper") {
-    target.src = "./assets/swiper1.png";
-    target.alt = "Swiper";
-    transition = "transform 5s ease-in-out";
-    if (Math.random() > 0.7) {
-      transition = "transform 2s linear";
-    }
-  } else if (targetType === "goldSwiper") {
-    target.style.height = "300px";
-    target.style.top = Math.random() * gameAreaHeight / 2 + "px";
-    target.src = "./assets/zalvin1.png";
-    target.alt = "Zalvin";
-    transition = "transform 2s ease-in-out";
-  } else if (targetType === "boots") {
-    target.src = "./assets/boots1.webp";
-    target.alt = "Boots";
-    transition = "transform 1.7s linear";
+  if (Math.random() > 0.7) {
+    transition = "transform 2s linear";
   }
-
   let spawnSide = "left";
   if (Math.random() > 0.5) {
     spawnSide = "right";
-    if (targetType === "swiper") {
-      target.src = "./assets/swiper1_flipped.png";
-    } else if (targetType === "goldSwiper") {
-      target.src = "./assets/zalvin1_flipped.png";
-    } else if (targetType === "boots") {
-      target.src = "./assets/boots1_flipped.webp";
-    }
+    target.src = "./assets/swiper1_flipped.png";
   }
-
   if (spawnSide === "left") {
     target.style.left = "-200px";
     setTimeout(() => {
@@ -71,22 +49,11 @@ function createTarget(targetType) {
     }
     target.dataset.clicked = "true";
     let currentPosition = window.getComputedStyle(target).transform;
-    if (targetType === "swiper") {
-      target.src = "./assets/swiper2.png";
-      currentScore += swiperPoints;
-    } else if (targetType === "goldSwiper") {
-      target.src = "./assets/zalvin2.png";
-      target.style.height = "200vh";
-      currentScore += goldSwiperPoints;
-      let zalvinSound = new Audio("./assets/zalvin.mp3");
-      zalvinSound.play();
-    } else if (targetType === "boots") {
-      target.src = "./assets/boots2.png";
-      currentScore += bootsPoints;
-    }
+    target.src = "./assets/swiper2.png";
     target.style.transform = currentPosition;
     target.style.transition = "opacity 1s ease-in";
     target.style.opacity = 0;
+    currentScore += swiperPoints;
     score.textContent = currentScore;
 
     setTimeout(() => {
@@ -104,6 +71,170 @@ function createTarget(targetType) {
   gameArea.appendChild(target);
 }
 
+function createBoots() {
+  let target = document.createElement("img");
+  target.style.position = "absolute";
+  target.style.top = Math.random() * gameAreaHeight + "px";
+  target.style.height = "150px";
+  target.src = "./assets/boots1.webp";
+  target.alt = "Boots";
+  let transition = "transform 1.7s linear";
+
+  let spawnSide = "left";
+  if (Math.random() > 0.5) {
+    spawnSide = "right";
+    target.src = "./assets/boots1_flipped.webp";
+  }
+  if (spawnSide === "left") {
+    target.style.left = "-200px";
+    setTimeout(() => {
+      target.style.transition = transition;
+      target.style.transform = "translateX(calc(100vw + 200px))";
+    }, 100);
+  } else {
+    target.style.right = "-200px";
+    setTimeout(() => {
+      target.style.transition = transition;
+      target.style.transform = "translateX(calc(-100vw - 200px))";
+    }, 100);
+  }
+
+  target.addEventListener("mousedown", function handleMouseDown() {
+    if (target.dataset.clicked) {
+      return;
+    }
+    target.dataset.clicked = "true";
+    let currentPosition = window.getComputedStyle(target).transform;
+    target.src = "./assets/boots2.png";
+    target.style.transform = currentPosition;
+    target.style.transition = "opacity 1s ease-in";
+    target.style.opacity = 0;
+    currentScore += bootsPoints;
+    score.textContent = currentScore;
+
+    setTimeout(() => {
+      target.remove();
+    }, 1000);
+  });
+
+  setTimeout(() => {
+    target.remove();
+  }, 5000);
+
+  target.addEventListener("dragstart", function (event) {
+    event.preventDefault();
+  });
+  gameArea.appendChild(target);
+}
+
+function createZalvin() {
+  let target = document.createElement("img");
+  target.style.position = "absolute";
+  target.style.top = Math.random() * gameAreaHeight + "px";
+  target.style.height = "150px";
+  target.style.height = "300px";
+  target.style.top = (Math.random() * gameAreaHeight) / 2 + "px";
+  target.src = "./assets/zalvin1.png";
+  target.alt = "Zalvin";
+  let transition = "transform 2s ease-in-out";
+
+  let spawnSide = "left";
+  if (Math.random() > 0.5) {
+    spawnSide = "right";
+    target.src = "./assets/zalvin1_flipped.png";
+  }
+  if (spawnSide === "left") {
+    target.style.left = "-200px";
+    setTimeout(() => {
+      target.style.transition = transition;
+      target.style.transform = "translateX(calc(100vw + 200px))";
+    }, 100);
+  } else {
+    target.style.right = "-200px";
+    setTimeout(() => {
+      target.style.transition = transition;
+      target.style.transform = "translateX(calc(-100vw - 200px))";
+    }, 100);
+  }
+
+  target.addEventListener("mousedown", function handleMouseDown() {
+    if (target.dataset.clicked) {
+      return;
+    }
+    target.dataset.clicked = "true";
+    currentScore += zalvinPoints;
+    score.textContent = currentScore;
+    target.remove();
+    let zalvinSound = new Audio("./assets/zalvin.mp3");
+    zalvinSound.play();
+
+    let jumpscare = document.createElement("img");
+    jumpscare.src = "./assets/zalvin2.png";
+    jumpscare.style.height = "200%";
+    jumpscare.style.position = "absolute";
+    jumpscare.style.left = "50%";
+    jumpscare.style.top = "100%";
+    jumpscare.style.transform = "translate(-50%, -50%)";
+    gameArea.appendChild(jumpscare);
+
+    setTimeout(() => {
+      jumpscare.remove();
+    }, 1000);
+  });
+
+  setTimeout(() => {
+    target.remove();
+  }, 5000);
+
+  target.addEventListener("dragstart", function (event) {
+    event.preventDefault();
+  });
+  gameArea.appendChild(target);
+}
+
+let swiperSpawned = 0;
+let swiperSpawnInterval = (totalTime / totalSwiper) * 1000;
+let spawnSwiper = setInterval(() => {
+  createSwiper();
+  swiperSpawned++;
+  if (swiperSpawned === totalSwiper) {
+    clearInterval(spawnSwiper);
+  }
+}, swiperSpawnInterval);
+
+let bootsSpawned = 0;
+let bootsSpawnInterval = (totalTime / totalBoots) * 1000;
+let spawnBoots = setInterval(() => {
+  createBoots();
+  bootsSpawned++;
+  if (bootsSpawned === totalBoots) {
+    clearInterval(spawnBoots);
+  }
+}, bootsSpawnInterval);
+
+let zalvinSpawned = 0;
+let zalvinSpawnInterval = (totalTime / totalZalvin) * 1000;
+let spawnZalvin = setInterval(() => {
+  createZalvin();
+  zalvinSpawned++;
+  if (zalvinSpawned === totalZalvin) {
+    clearInterval(spawnZalvin);
+  }
+}, zalvinSpawnInterval);
+
+let gun = document.getElementById("gun");
+let offsetX = 200;
+document.addEventListener("mousemove", (event) => {
+  let mouseX = event.clientX;
+  let gunPosition = mouseX + offsetX;
+  gun.style.transform = `translateX(${gunPosition}px)`;
+});
+
+gameArea.addEventListener("mousedown", () => {
+  let gunSound = new Audio("./assets/ak47_sound.webm");
+  gunSound.play();
+});
+
 let timeLeft = totalTime;
 setTimeout(() => {
   let timerCountdown = setInterval(() => {
@@ -114,50 +245,6 @@ setTimeout(() => {
     }
   }, 1000);
 }, 500);
-
-let swiperSpawned = 0;
-let swiperSpawnInterval = (totalTime / totalSwiper) * 1000;
-let spawnSwiper = setInterval(() => {
-  createTarget("swiper");
-  swiperSpawned++;
-  if (swiperSpawned === totalSwiper) {
-    clearInterval(spawnSwiper);
-  }
-}, swiperSpawnInterval);
-
-let goldSwiperSpawned = 0;
-let goldSwiperSpawnInterval = (totalTime / totalGoldSwiper) * 1000;
-let spawnGoldSwiper = setInterval(() => {
-  createTarget("goldSwiper");
-  goldSwiperSpawned++;
-  if (goldSwiperSpawned === totalGoldSwiper) {
-    clearInterval(spawnGoldSwiper);
-  }
-}, goldSwiperSpawnInterval);
-
-let bootsSpawned = 0;
-let bootsSpawnInterval = (totalTime / totalBoots) * 1000;
-let spawnBoots = setInterval(() => {
-  createTarget("boots");
-  bootsSpawned++;
-  if (bootsSpawned === totalBoots) {
-    clearInterval(spawnBoots);
-  }
-}, bootsSpawnInterval);
-
-let gameArea = document.getElementById("game-area");
-gameArea.addEventListener("mousedown", () => {
-  let gunSound = new Audio("./assets/ak47_sound.webm");
-  gunSound.play();
-});
-
-const gun = document.getElementById("gun");
-const offsetX = 200;
-document.addEventListener("mousemove", (event) => {
-  let mouseX = event.clientX;
-  let gunPosition = mouseX + offsetX;
-  gun.style.transform = `translateX(${gunPosition}px)`;
-});
 
 function showCongratulations() {
   document.getElementById("final-score").textContent = currentScore;
